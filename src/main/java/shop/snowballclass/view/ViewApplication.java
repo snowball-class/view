dc57a96a8eca0378b1fca13c25ce7acb0870dda9
Chanui <jcu011@naver.com>
2025-03-03 19:59:07 +0900

    refactor: @EnableFeignClients 추가

--- src/main/java/shop/snowballclass/view/ViewApplication.java
+++ src/main/java/shop/snowballclass/view/ViewApplication.java
@@ -2,7 +2,9 @@ package shop.snowballclass.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ViewApplication {
 
