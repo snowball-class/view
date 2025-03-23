70341da34c2ba38b4a8332c6f95ff32cae206293
Chanui <jcu011@naver.com>
2025-03-23 19:45:45 +0900

    feat: member Client 추가

---
+++ src/main/java/shop/snowballclass/view/client/MemberClient.java
@@ -0,0 +1,15 @@
+package shop.snowballclass.view.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import shop.snowballclass.view.dto.ApiResponse;
+import shop.snowballclass.view.dto.member.MemberInfoResponse;
+
+@FeignClient(name = "lesson-service", url = "${feign.snowball.member}")
+public interface MemberClient {
+
+    @GetMapping
+    ApiResponse<MemberInfoResponse> getMemberInfo(@RequestHeader("Authorization") String token);
+
+}
